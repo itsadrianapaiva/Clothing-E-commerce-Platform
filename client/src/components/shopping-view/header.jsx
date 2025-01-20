040deaf1f515132fdacb0904cc90f618ba62af32
unknown <adrianapaivausa@icloud.com>
2025-01-19 19:08:44 -0500

    account card and logic added

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -1,13 +1,75 @@
-import { House, Menu } from "lucide-react";
-import { Link } from "react-router-dom";
+import { House, LogOut, Menu, ShoppingCart, User } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 import { Button } from "../ui/button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { shoppingViewHeaderMenuItems } from "@/config";
+import {
+  DropdownMenu,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from "../ui/dropdown-menu";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+import { logoutUser } from "@/store/auth-slice";
 
 function MenuItems() {
-  return <nav>
-    
-  </nav>
+  return (
+    <nav className="flex flex-col mb-3 lg:mb-0 lg:items-center gap-6 lg:flex-row">
+      {shoppingViewHeaderMenuItems.map((menuItem) => (
+        <Link
+          className="text-sm font-medium cursor-pointer"
+          key={menuItem.id}
+          to={menuItem.path}
+        >
+          {menuItem.label}
+        </Link>
+      ))}
+    </nav>
+  );
+}
+
+function HeaderRightContent() {
+  const { user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  function handleLogout() {
+    dispatch(logoutUser());
+  }
+
+  return (
+    <div className="flex lg:items-center lg:flex-row flex-col gap-4">
+      <Button variant="outline" size="icon" className="relative">
+        <ShoppingCart className="h-6 w-6" />
+        <span className="sr-only">User cart</span>
+      </Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar className="bg-black">
+            <AvatarFallback className="bg-black text-white font-extrabold">
+              {user?.userName[0].toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent side="right" className="w-56">
+          <DropdownMenuLabel>Logged in as {user?.userName}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={() => navigate("/shop/account")}>
+            <User className="h-4 w-4 mr-2" />
+            Account
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={handleLogout}>
+            <LogOut className="h-4 w-4 mr-2" />
+            Logout
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
 }
 
 export default function ShoppingHeader() {
@@ -27,10 +89,18 @@ export default function ShoppingHeader() {
               <span className="sr-only">Toggle header menu</span>
             </Button>
           </SheetTrigger>
-          <SheetContent side="left" className="w-full max-w-xs"></SheetContent>
+          <SheetContent side="left" className="w-full max-w-xs">
+            <MenuItems />
+          </SheetContent>
         </Sheet>
-        <div className="hidden lg:block"></div>
-        {isAuthenticated ? <div></div> : null}
+        <div className="hidden lg:block">
+          <MenuItems />
+        </div>
+        {isAuthenticated ? (
+          <div>
+            <HeaderRightContent />
+          </div>
+        ) : null}
       </div>
     </header>
   );
--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -67,6 +67,7 @@ const loginUser = async (req, res) => {
         id: checkUser._id,
         role: checkUser.role,
         email: checkUser.email,
+        userName: checkUser.userName,
       },
       "CLIENT_SECRET_KEY",
       { expiresIn: "60m" }
@@ -84,6 +85,7 @@ const loginUser = async (req, res) => {
           email: checkUser.email,
           role: checkUser.role,
           id: checkUser._id,
+          userName: checkUser.userName,
         },
       });
   } catch (error) {
